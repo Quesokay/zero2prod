5a2bde0c58b3aa7227ccfce080ec4b8e86e3f40b
Victor Moyo <ques4235@gmail.com>
2026-04-25 11:55:50 +0200

    Implemented first integration test.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> Result<(), std :: io :: Error> {
-    run().await
+async fn main() -> Result<(), std::io::Error> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await
 }
