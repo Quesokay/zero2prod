38fe5c8883f16553fd13cef13377f6205245f49b
Victor Moyo <ques4235@gmail.com>
2026-04-25 13:30:08 +0200

    Parsing Form Data from a POST Request

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,22 @@ async fn health_check() -> HttpResponse{
     HttpResponse :: Ok().finish()
 }
 
-// Notice the different signature!
-// We return `Server` on the happy path and we dropped the `async` keyword
-// We have no .await call, so it is not needed anymore.
+#[derive(serde::Deserialize)]
+struct FormData{
+    email: String,
+    name: String
+}
+
+// Let's start simple: we always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new( ||{
             App::new()
                 .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
         })
         .listen(listener)?
         .run();
