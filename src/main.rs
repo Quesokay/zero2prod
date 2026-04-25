d788edc85f7cfe2bb9b0a467ac65846790c4afe6
Victor Moyo <ques4235@gmail.com>
2026-04-25 12:24:55 +0200

    refactored integration test to use random ports

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    run(listener)?.await
 }
